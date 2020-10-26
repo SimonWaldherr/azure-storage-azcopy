0c3e387fee93b7784b280225e23d06b37c2e4bac
zezha-msft <zezha@microsoft.com>
2020-10-26 02:16:15 -0700

    Added frontend logging for Blob scanning

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.6.1"
+const AzcopyVersion = "10.6.1-sync-experiment"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
